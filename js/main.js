3ee3ba3810ebbffc9612b93ee42d800807c39273
mohiuddinmohi <mohiuddin0240@gmail.com>
2022-05-25 16:00:58 +0600

    style: format updated

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
-const items = document.querySelectorAll('#timeline li');
+const items = document.querySelectorAll("#timeline li");
 
 const isInViewport = (el) => {
+  console.log(el);
   const rect = el.getBoundingClientRect();
   console.log(rect);
   return (
     rect.top >= 0 &&
     rect.left >= 0 &&
-    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
     rect.right <= (window.innerWidth || document.documentElement.clientWidth)
   );
 };
@@ -14,11 +16,11 @@ const isInViewport = (el) => {
 const run = () =>
   items.forEach((item) => {
     if (isInViewport(item)) {
-      item.classList.add('show');
+      item.classList.add("show");
     }
   });
 
 // Events
-window.addEventListener('load', run);
-window.addEventListener('resize', run);
-window.addEventListener('scroll', run);
+window.addEventListener("load", run);
+window.addEventListener("resize", run);
+window.addEventListener("scroll", run);
